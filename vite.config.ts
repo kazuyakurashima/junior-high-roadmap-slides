3501bcb71797487ee61e13e8130e3d8c13dcbd66
kazuyakurashima <gs.kaz224@gmail.com>
2025-05-24 20:14:15 +0900

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: './', // ← ★これを追加！相対パスでリソースを探すようにする
   plugins: [react()],
   resolve: {
     alias: {
